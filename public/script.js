f0f22216535f5eccc2cc8b4004dccb79ce098c86
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-12-01 22:30:28 -0600

    feat: Add basic client handling of websocket data
    This is a basic example of handling web socket data, and putting it in
    the `messageContainer` div.

---
+++ public/script.js
@@ -0,0 +1,18 @@
+const ws = new WebSocket(`ws://${window.location.host}`);
+const messageContainer = document.getElementById('message-container');
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+
+ws.onmessage = function(event) {
+  console.log(event.data);
+  const reader = new FileReader();
+  reader.onload = function() {
+    var message = document.createElement('div');
+
+    const result = JSON.parse(reader.result);
+    message.textContent = result.text;
+
+    messageContainer.appendChild(message);
+  };
+  reader.readAsText(event.data);
+};
