cd61e6b9bd78a7c3c6f7d48f5b87eb064c289e25
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-12-02 21:54:08 -0600

    feat: Add a way to generate a random color
    This code selects a random hue and generates a color based on that hue.

--- public/script.js
+++ public/script.js
@@ -6,6 +6,37 @@ const keyButton = document.getElementById('key-button');
 
 var key = prompt('Enter key:');
 
+function hsv(h, s, v) {
+  var r, g, b, i, f, p, q, t;
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+
+  const toHex = (x) => {
+    const hex = Math.round(x * 255).toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+
+  return '#' + toHex(r) + toHex(g) + toHex(b);
+}
+
+function randomColor() {
+  return hsv(Math.random() * 360, 0.95, 0.95);
+}
+
+const myColor = randomColor();
+
 function setKey() {
   key = prompt('Enter key:');
   render();
